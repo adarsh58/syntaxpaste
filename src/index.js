d8a2b302771e1b2135447f57f7e06def33052eda
adarsh58 <adarsh.sharma58@yahoo.com>
2024-08-30 15:40:41 +0530

    Added Data

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import Index from './Demo/lazy/Index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Css/> */}
-     {/* <App />  */}
-     <Index/>
+
+     <App /> 
+     {/* <Index/> */}
   
   </React.StrictMode>
 );
