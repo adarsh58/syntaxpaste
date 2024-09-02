51ce392d27703d6b89f3aacf8e96f8bf10b53b91
adarsh58 <adarsh.sharma58@yahoo.com>
2024-09-02 16:38:59 +0530

    Added IndexDb

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -2041,81 +2041,108 @@ root.render(
       {
         Logic: "Usage",
         File: "App.jsx",
-        Syntax: `import React, { useState,useEffect } from "react";
+        Syntax: `import React, { useState, useEffect } from "react";
 import { useIndexedDB } from "react-indexed-db-hook";
 import "./Shop.css";
 const Shop = () => {
-const { add,clear,getAll  } = useIndexedDB("cart");
+  const { add, clear, getAll, deleteRecord } = useIndexedDB("cart");
 
-const [note, setNote] = useState({ name: "", pid: ""});
-const [noteDb, setNoteDb] = useState({ name: "", pid: ""});
+  const [note, setNote] = useState({ name: "", pid: "" });
+  const [noteDb, setNoteDb] = useState({ name: "", pid: "" });
 
-useEffect(() => {
-getAll().then((personsFromDB) => {
-setNoteDb(personsFromDB)
-});
-}, []);
+  useEffect(() => {
+    getAll().then((personsFromDB) => {
+      setNoteDb(personsFromDB);
+    });
+  }, []);
 
-const GetAll=()=>{
-getAll().then((personsFromDB) => {
-setNoteDb(personsFromDB)
-});
-}
-const Add=()=>{
+  const GetAll = () => {
+    getAll().then((personsFromDB) => {
+      setNoteDb(personsFromDB);
+    });
+  };
+  const Add = () => {
+    if (note.name.trim() !== "" && note.pid.trim() !== ""  ) {
+      add({ name: note.name, pid: note.pid }).then(
+        (event) => {
+          GetAll();
+        },
+        (error) => {
+          console.log("Error", error);
+        }
+      );
+    }
+  };
 
-add({ name: note.name, pid: note.pid }).then(
-(event) => {
-  GetAll();
+  const Delete = (id) => {
+    deleteRecord(id).then((event) => {
+      GetAll();
+    });
+  };
 
-},
-(error) => {
-  console.log("Error",error);
-},
-);
-}
-const Clear=()=>{
-clear().then(() => {
-setNoteDb([])
-
-});
-setNote({ name: "", pid: ""})
-} 
-const onChange = (e) => {
-setNote({ ...note, [e.target.name]: e.target.value });
-console.log(note,note.length)
-}
-return (
-<div className="container">
-
-<div className="form">
-  <div className="name">
-    <label>name</label>
-    <input onChange={onChange} name="name" type="text" value={note.name}></input>
-  </div>
-  <div className="pid">
-    <label>PId</label>
-    <input onChange={onChange} name="pid" type="text" value={note.pid}></input>
-  </div>
-  <div className="pid">
-   <button onClick={GetAll}>get all</button>
-   <button onClick={Add} >add</button>
-   <button onClick={Clear}>clear</button>
-  
-  </div>
-  <ul>
-  {
-     noteDb.length>0 && noteDb.map((e,i)=>{
-          return (<li>name : {e.name} Id : {e.pid}</li>)
-    })
-  }
-  </ul>
-
-</div>
-</div>
-);
+  const Clear = () => {
+    clear().then(() => {
+      setNoteDb([]);
+    });
+    setNote({ name: "", pid: "" });
+  };
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+    console.log(note, note.length);
+  };
+  return (
+    <div className="container">
+      <div className="form">
+        <h2>IndexDB working</h2>
+        <div className="text">
+          <label className="label">Name</label> &nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            onChange={onChange}
+            name="name"
+            type="text"
+            value={note.name}
+          ></input>
+        </div>
+        <div className="text">
+          <label className="label">PrId</label>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            onChange={onChange}
+            name="pid"
+            type="text"
+            value={note.pid}
+          ></input>
+        </div>
+        <div className="button">
+          <button className="btn btn-info" onClick={GetAll}>
+            get all
+          </button>
+          <button className="btn btn-warning" onClick={Add}>
+            add
+          </button>
+          <button className="btn btn-secondary" onClick={Clear}>
+            clear
+          </button>
+       
+        </div>
+        <ul>
+          {noteDb &&
+            noteDb.length > 0 &&
+            noteDb.map((e, i) => {
+              return (
+                <li> 
+                  ID : {e.id} <br></br> Name : {e.name} <br></br> ProductId : {e.pid}{" "}<i onClick={()=>Delete(e.id)} class="bi bi-trash3"></i> <br></br>
+                   </li>
+              );
+            })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
+
 `
     }
   ]
