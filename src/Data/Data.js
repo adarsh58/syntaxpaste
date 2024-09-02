4c72e4c95761cf8d13becb5f262130a8f16267c8
adarsh58 <adarsh.sharma58@yahoo.com>
2024-09-02 16:19:27 +0530

    Added IndexDb

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1997,5 +1997,127 @@ grid-template-areas: 'navbar'
 </div>`
       }
   ]
+}, {
+  Category: REACT,
+  Concept: "Index DB to store data in browser/Disk",
+  Img: reactImg,
+  Code: [
+      {
+          Logic: "npm install react-indexed-db-hook npm i react-indexed-db",
+          File: "DBConfig.js",
+          Syntax: `export const DBConfig = {
+name: "CartMe",
+version: 1,
+objectStoresMeta: [
+{
+  store: "cart",
+  storeConfig: { keyPath: "id", autoIncrement: true },
+  storeSchema: [
+    { name: "name", keypath: "name", options: { unique: false } },
+    { name: "pid", keypath: "pid", options: { unique: false } },
+  ],
+},
+],
+}`
+      },{
+          Logic: "App root",
+          File: "Index.jsx",
+          Syntax: `import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { DBConfig } from "../src/IndexDb/Dbconfig";
+import { initDB } from "react-indexed-db-hook";
+
+initDB(DBConfig);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);`
+      },
+      {
+        Logic: "Usage",
+        File: "App.jsx",
+        Syntax: `import React, { useState,useEffect } from "react";
+import { useIndexedDB } from "react-indexed-db-hook";
+import "./Shop.css";
+const Shop = () => {
+const { add,clear,getAll  } = useIndexedDB("cart");
+
+const [note, setNote] = useState({ name: "", pid: ""});
+const [noteDb, setNoteDb] = useState({ name: "", pid: ""});
+
+useEffect(() => {
+getAll().then((personsFromDB) => {
+setNoteDb(personsFromDB)
+});
+}, []);
+
+const GetAll=()=>{
+getAll().then((personsFromDB) => {
+setNoteDb(personsFromDB)
+});
+}
+const Add=()=>{
+
+add({ name: note.name, pid: note.pid }).then(
+(event) => {
+  GetAll();
+
+},
+(error) => {
+  console.log("Error",error);
+},
+);
+}
+const Clear=()=>{
+clear().then(() => {
+setNoteDb([])
+
+});
+setNote({ name: "", pid: ""})
+} 
+const onChange = (e) => {
+setNote({ ...note, [e.target.name]: e.target.value });
+console.log(note,note.length)
+}
+return (
+<div className="container">
+
+<div className="form">
+  <div className="name">
+    <label>name</label>
+    <input onChange={onChange} name="name" type="text" value={note.name}></input>
+  </div>
+  <div className="pid">
+    <label>PId</label>
+    <input onChange={onChange} name="pid" type="text" value={note.pid}></input>
+  </div>
+  <div className="pid">
+   <button onClick={GetAll}>get all</button>
+   <button onClick={Add} >add</button>
+   <button onClick={Clear}>clear</button>
+  
+  </div>
+  <ul>
+  {
+     noteDb.length>0 && noteDb.map((e,i)=>{
+          return (<li>name : {e.name} Id : {e.pid}</li>)
+    })
+  }
+  </ul>
+
+</div>
+</div>
+);
+};
+
+export default Shop;
+`
+    }
+  ]
 }
 ]
--- src/Data/DataTemplate.js
+++ src/Data/DataTemplate.js
@@ -2,88 +2,125 @@
 export const Data = [
     {
         Category: REACT,
-        Concept: "CSS GRID Responsive Template ",
+        Concept: "Index DB to store data in browser/Disk",
         Img: reactImg,
         Code: [
             {
-                Logic: "",
-                File: "Style.css",
-                Syntax: `.containerGrid
-{
-height: 100vh;
-display: grid;
-font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-grid-template-areas: 'navbar navbar navbar '
-                      'sidebar main main'
-                      'sidebar box1 box2'
-                      'sidebar footer footer';
-                      grid-gap: .6rem;
-                      text-align: center;
-                      grid-template-columns: .8fr 1fr 1fr;
-                      grid-template-rows: 1.8fr 2.5fr 1.5fr .8fr;
-
-} 
-nav{
-  grid-area: navbar;
-  background-color: lightgreen;
-}
-.main
-{
-  grid-area: main;
-  background-color: lightcoral;
-}
-.sidebar
-{
-  grid-area: sidebar;
-  background-color: rgb(132, 128, 240);
-}
-footer
-{
-  grid-area: footer;
-  background-color: rgb(16, 155, 99);
-}
-.box1
-{
-  grid-area: box1;
-  background-color: rgb(155, 16, 16);
-}
-.box2
-{
-  grid-area: box2;
-  background-color: rgb(130, 155, 16);
-}
-
-@media (max-width: 640px) {
-  .containerGrid
-  {
-    height: 100vh;
-  display: grid;
-  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-  grid-template-areas: 'navbar'
-                        'sidebar'
-                        'main'
-                        'box1'
-                        'box2'
-                        'footer';
-                        grid-gap: .6rem;
-                        text-align: center;
-                        grid-template-columns:1fr;
-                        grid-template-rows: 1.8fr 2.5fr 1.5fr 1.5fr .8fr;
-  
-  } 
-}`
+                Logic: "npm install react-indexed-db-hook npm i react-indexed-db",
+                File: "DBConfig.js",
+                Syntax: `export const DBConfig = {
+    name: "CartMe",
+    version: 1,
+    objectStoresMeta: [
+      {
+        store: "cart",
+        storeConfig: { keyPath: "id", autoIncrement: true },
+        storeSchema: [
+          { name: "name", keypath: "name", options: { unique: false } },
+          { name: "pid", keypath: "pid", options: { unique: false } },
+        ],
+      },
+    ],
+  }`
             },{
-                Logic: "",
+                Logic: "App root",
                 File: "Index.jsx",
-                Syntax: ` <div className="containerGrid">
-      <nav>NavBar</nav>
-      <div className="main">main</div>
-      <div className="sidebar">sidebar</div>
-      <div className="box1">box 1</div>
-      <div className="box2">box 2</div>
-      <footer>Footer</footer>
-    </div>`
-            }
+                Syntax: `import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { DBConfig } from "../src/IndexDb/Dbconfig";
+import { initDB } from "react-indexed-db-hook";
+
+initDB(DBConfig);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);`
+            },
+            {
+              Logic: "Usage",
+              File: "App.jsx",
+              Syntax: `import React, { useState,useEffect } from "react";
+import { useIndexedDB } from "react-indexed-db-hook";
+import "./Shop.css";
+const Shop = () => {
+  const { add,clear,getAll  } = useIndexedDB("cart");
+
+  const [note, setNote] = useState({ name: "", pid: ""});
+  const [noteDb, setNoteDb] = useState({ name: "", pid: ""});
+ 
+  useEffect(() => {
+    getAll().then((personsFromDB) => {
+      setNoteDb(personsFromDB)
+    });
+  }, []);
+ 
+  const GetAll=()=>{
+    getAll().then((personsFromDB) => {
+      setNoteDb(personsFromDB)
+     });
+  }
+  const Add=()=>{
+  
+    add({ name: note.name, pid: note.pid }).then(
+      (event) => {
+        GetAll();
+   
+      },
+      (error) => {
+        console.log("Error",error);
+      },
+    );
+  }
+  const Clear=()=>{
+    clear().then(() => {
+      setNoteDb([])
+      
+    });
+    setNote({ name: "", pid: ""})
+  } 
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+    console.log(note,note.length)
+}
+  return (
+    <div className="container">
+    
+      <div className="form">
+        <div className="name">
+          <label>name</label>
+          <input onChange={onChange} name="name" type="text" value={note.name}></input>
+        </div>
+        <div className="pid">
+          <label>PId</label>
+          <input onChange={onChange} name="pid" type="text" value={note.pid}></input>
+        </div>
+        <div className="pid">
+         <button onClick={GetAll}>get all</button>
+         <button onClick={Add} >add</button>
+         <button onClick={Clear}>clear</button>
+        
+        </div>
+        <ul>
+        {
+           noteDb.length>0 && noteDb.map((e,i)=>{
+                return (<li>name : {e.name} Id : {e.pid}</li>)
+          })
+        }
+        </ul>
+  
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
+`
+          }
         ]
     }
 
